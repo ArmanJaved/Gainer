cdf362a34816958fd2c60d8a6ce6cae08793498b
ArmanBrain <marmanjaved@gmail.com>
2017-12-23 15:10:53 +0500

    asdf
    assaf

--- callme/gai.py
+++ callme/gai.py
@@ -18,7 +18,7 @@ import os
 #
 
 GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
-CHROMEDRIVER_PATH = './chromedriver.exe'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
 chrome_options = Options()
 chrome_options.binary_location = GOOGLE_CHROME_BIN
