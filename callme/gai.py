b0b03c1bae8b093897ccdc35c81d7e707a6cf206
ArmanBrain <marmanjaved@gmail.com>
2017-12-26 14:08:56 +0500

    asdfasf
    afsasafs

--- callme/gai.py
+++ callme/gai.py
@@ -6,26 +6,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gainers.settings")
 
 from bs4 import BeautifulSoup
 import requests
-import time
-from selenium.webdriver.chrome.options import Options
-### TOR crawling
-
-from selenium import webdriver
-from selenium.webdriver.common.proxy import Proxy, ProxyType
-import os
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# CHROMEDRIVER_PATH = "./chromedriver.exe"
-#
-
-GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-
-chrome_options = Options()
-chrome_options.binary_location = GOOGLE_CHROME_BIN
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-
-
 
 
 def removeDoubleSpace(st):
@@ -36,17 +16,15 @@ def removeDoubleSpace(st):
     x1 = str(re.sub(r"\n", "", str(x)))
     return x1
 def mainCategory(url1, firebase):
-
-
+    print("Ali ")
     for i in range(1, 11):
-        firebase.delete('/Ticker', i)
+        firebase.delete('/Gainers', i)
         pass
 
-    browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-    # browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)=
-    browser.get(str(url1))
-    soup = BeautifulSoup((browser.page_source).encode('utf-8'), 'html.parser')
 
+
+    page =  str(requests.get(str(url1)).content)
+    soup = BeautifulSoup(str(page), 'html.parser')
 
 
     categoryList = soup.findAll('tr', attrs={'valign': 'top'})
@@ -54,10 +32,6 @@ def mainCategory(url1, firebase):
     dic_list = []
     num = 0
 
-
-    # firebase = firebase.FirebaseApplication('https://stockapp-238b6.firebaseio.com/', None)
-    # result = firebase.get('/users', None)
-    # print (result)
     for category in categoryList:
         lists = category.findAll('td')
         count = 0
@@ -89,7 +63,7 @@ def mainCategory(url1, firebase):
             dic_list.append(dic)
             print(dic)
 
-            result = firebase.put('/Ticker', num, abs)
+            result = firebase.put('/Gainers', num, abs)
             print(result)
         num = num + 1
     return dic_list
@@ -98,6 +72,4 @@ def mainCategory(url1, firebase):
 def main():
     from firebase import firebase
     firebase = firebase.FirebaseApplication('https://stockapp-238b6.firebaseio.com/', None)
-    mainCategory('https://finviz.com/screener.ashx?v=110&s=ta_toplosers', firebase)
-
-main()
+    mainCategory('https://finviz.com/screener.ashx?v=110&s=ta_topgainers', firebase)
