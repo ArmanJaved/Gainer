d7a7ec9ceed83ad2935e6f3541aa1d8abd1efb21
ArmanBrain <marmanjaved@gmail.com>
2018-04-30 20:03:32 +0500

    asdfasdf

--- callme/urls.py
+++ callme/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'gainers', views.gainers, name='gainers'),
     url(r'losers', views.losers, name='losers'),
+    url(r'multop', views.multop, name='multop'),
+    url(r'mulbot', views.mulbot, name='mulbot'),
+    url(r'consol', views.consol, name='consol'),
 ]
--- callme/views.py
+++ callme/views.py
@@ -2,6 +2,10 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from callme import gai
 from callme import los
+from callme import mulb
+from callme import mult
+from callme import conso
+
 
 from django.shortcuts import render
 
@@ -20,4 +24,26 @@ def losers(request):
     var1 = request.get_full_path()
     print (var1)
     los.main()
+    return HttpResponse("<h1> Done !!! </h1>")
+
+def multop(request):
+
+    var1 = request.get_full_path()
+    print (var1)
+    mult.main()
+    return HttpResponse("<h1> Done !!! </h1>")
+
+
+def mulbot(request):
+
+    var1 = request.get_full_path()
+    print (var1)
+    mulb.main()
+    return HttpResponse("<h1> Done !!! </h1>")
+
+def consol(request):
+
+    var1 = request.get_full_path()
+    print (var1)
+    conso.main()
     return HttpResponse("<h1> Done !!! </h1>")
